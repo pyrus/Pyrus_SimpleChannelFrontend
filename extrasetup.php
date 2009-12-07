0b95111f65a691c83eee75b7f555fb42067aa21b
Brett Bieber <brett.bieber@gmail.com>
2009-12-07 04:49:01 +0000

    Update package release notes and package.xml, fix paths in extrasetup to work from checkout of pear2/all

--- extrasetup.php
+++ extrasetup.php
@@ -2,7 +2,7 @@
 /**
  * This file is used to provide extra files/packages outside package.xml
  */
-$pyrus = new \pear2\Pyrus\Package(__DIR__ . '/../PEAR2/Pyrus/package.xml');
+$pyrus = new \pear2\Pyrus\Package(__DIR__ . '/../../Pyrus/package.xml');
 $pyrus->setPackagingFilter('pear2\Pyrus\PackageFile\v2Iterator\MinimalPackageFilter');
 if (basename(__DIR__) == 'trunk') {
     $extrafiles = array(
@@ -13,12 +13,12 @@ if (basename(__DIR__) == 'trunk') {
     );
 } else {
     $extrafiles = array(
-        new \pear2\Pyrus\Package(__DIR__ . '/../PEAR2/HTTP_Request/package.xml'),
-        new \pear2\Pyrus\Package(__DIR__ . '/../PEAR2/sandbox/Console_CommandLine/package.xml'),
-        new \pear2\Pyrus\Package(__DIR__ . '/../PEAR2/MultiErrors/package.xml'),
-        new \pear2\Pyrus\Package(__DIR__ . '/../PEAR2/Exception/package.xml'),
-        new \pear2\Pyrus\Package(__DIR__ . '/../PEAR2/Exception/package.xml'),
+        new \pear2\Pyrus\Package(__DIR__ . '/../../HTTP_Request/package.xml'),
+        new \pear2\Pyrus\Package(__DIR__ . '/../Console_CommandLine/package.xml'),
+        new \pear2\Pyrus\Package(__DIR__ . '/../../MultiErrors/package.xml'),
+        new \pear2\Pyrus\Package(__DIR__ . '/../../Exception/package.xml'),
+        new \pear2\Pyrus\Package(__DIR__ . '/../../Exception/package.xml'),
         $pyrus,
-        new \pear2\Pyrus\Package(__DIR__ . '/../PEAR2/Templates_Savant/package.xml'),
+        new \pear2\Pyrus\Package(__DIR__ . '/../../Templates_Savant/package.xml'),
     );
 }
