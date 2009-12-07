7e593f7fe67d5df754c5ea129ae6993ea23724ff
Brett Bieber <brett.bieber@gmail.com>
2009-12-07 04:42:27 +0000

    Add latest version number to the install command so regardless of stability package will be installed.

--- www/templates/Package.tpl.php
+++ www/templates/Package.tpl.php
@@ -14,7 +14,7 @@ $parent->context->page_title = $context->name.' | '.pear2\SimpleChannelFrontend\
         <h3>Installation</h3>
         <ol class="instructions">
             <li><code>$>php pyrus.phar channel-discover <?php echo $context->channel; ?></code></li>
-            <li><code>$>php pyrus.phar install <?php echo $context->channel . '/' . $context->name; ?></code></li>
+            <li><code>$>php pyrus.phar install <?php echo $context->channel . '/' . $context->name . '-' . $context->version['release']; ?></code></li>
         </ol>
     </div>
     <div class="grid_4 right releases">
