7fa8ed48fee4de92be8827610f08a7433f9c6dbc
Michael Gauthier <gauthierm@php.net>
2010-07-05 19:31:51 +0000

    Clean up release file browser a little bit. Trailing slash on Main::channel_path is now optional.

--- src/SimpleChannelFrontend/ReleaseFileBrowser.php
+++ src/SimpleChannelFrontend/ReleaseFileBrowser.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace PEAR2\SimpleChannelFrontend;
+
 class ReleaseFileBrowser
 {
     public $options;
@@ -8,12 +10,14 @@ class ReleaseFileBrowser
 
     public $internal_file;
 
-    function __construct($options = array())
+    public function __construct($options = array())
     {
-        $file = Main::$channel_path.'/get/'.$options['release'];
-        if (!file_exists($file)
-            || dirname($file) != Main::$channel_path.'/get') {
-            throw new \Exception('Cannot find the package. '.$file);
+        $root = rtrim(Main::$channel_path, DIRECTORY_SEPARATOR);
+        $root = $root . DIRECTORY_SEPARATOR . 'get';
+        $file = $root . DIRECTORY_SEPARATOR . $options['release'];
+
+        if (!file_exists($file) || dirname($file) != $root) {
+            throw new \Exception('Cannot find the package ' . $file . '.');
         }
 
         $this->release = new \PharData($file);
