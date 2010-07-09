36c28b2216188a65bdbbaf3aa34ff1b96325bc8b
Michael Gauthier <gauthierm@php.net>
2010-07-09 19:54:06 +0000

    Changes to SCF's file browser.

--- src/SimpleChannelFrontend/ReleaseFileBrowser.php
+++ src/SimpleChannelFrontend/ReleaseFileBrowser.php
@@ -4,28 +4,56 @@ namespace PEAR2\SimpleChannelFrontend;
 
 class ReleaseFileBrowser
 {
+    public $package;
+
     public $options;
 
-    public $release;
+    public $releaseFile;
 
-    public $internal_file;
+    public $file;
 
     public function __construct($options = array())
     {
+        $channel       = $options['frontend']::$channel;
+        $this->package = $channel->remotepackage[$options['package']];
+
+        if (isset($options['packageVersion'])) {
+            // try to set specified version
+            try {
+                $this->package->setRawVersion(
+                    null,
+                    array('release' => $options['packageVersion'])
+                );
+            } catch (\Exception $e) {
+                throw new UnregisteredViewException($e->getMessage());
+            }
+        } else {
+            // set lastest release version
+            $this->package->rewind();
+            $this->package->setRawVersion(
+                null,
+                array('release' => $this->package->key())
+            );
+        }
+
+        $releaseFile = $this->package->name . '-'
+            . $this->package->version['release'] . '.tgz';
+
+
         $root = rtrim(Main::$channel_path, DIRECTORY_SEPARATOR);
         $root = $root . DIRECTORY_SEPARATOR . 'get';
-        $file = $root . DIRECTORY_SEPARATOR . $options['release'];
+        $file = $root . DIRECTORY_SEPARATOR . $releaseFile;
 
         if (!file_exists($file) || dirname($file) != $root) {
             throw new \Exception('Cannot find the package ' . $file . '.');
         }
 
-        $this->release = new \PharData($file);
+        $this->releaseFile = new \PharData($file);
 
-        if (isset($options['internal'])) {
-            $file = 'phar://'.$file.'/'.$options['internal'];
+        if (isset($options['file'])) {
+            $file = 'phar://' . $file . '/' . $options['file'];
             if (file_exists($file)) {
-                $this->internal_file = new \SplFileObject($file);
+                $this->file = new \SplFileObject($file);
             }
         }
 
