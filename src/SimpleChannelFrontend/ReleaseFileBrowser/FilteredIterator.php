27bf96c736a7263e6ccc0b59ec184d2a25f76574
Brett Bieber <brett.bieber@gmail.com>
2010-07-13 04:10:06 +0000

    Remove unneeded constructor.

--- src/SimpleChannelFrontend/ReleaseFileBrowser/FilteredIterator.php
+++ src/SimpleChannelFrontend/ReleaseFileBrowser/FilteredIterator.php
@@ -3,11 +3,6 @@ namespace PEAR2\SimpleChannelFrontend\ReleaseFileBrowser;
 
 class FilteredIterator extends \RecursiveFilterIterator
 {
-    function __construct($iterator)
-    {
-        parent::__construct($iterator);
-    }
-
     function accept()
     {
         return !($this->getInnerIterator()->getBasename() == '.xmlregistry');
