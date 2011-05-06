33117d5da7de86bff9189f8367c416618d2cf26a
Brett Bieber <brett.bieber@gmail.com>
2011-05-06 15:54:18 -0500

    Use $frontend to reference the frontend.

--- www/templates/html/PackageRelease.tpl.php
+++ www/templates/html/PackageRelease.tpl.php
@@ -1,11 +1,11 @@
 <?php
 // Set the title for the main template
-$parent->context->page_title = $context->name.' | '.PEAR2\SimpleChannelFrontend\Main::$channel->name;
+$parent->context->page_title = $context->name.' | '.$frontend->getChannel()->name;
 ?>
 <div class="package">
     <div class="grid_8 left">
         <h2>
-            <a href="<?php echo PEAR2\SimpleChannelFrontend\Main::getURL() . $context->name; ?>"><?php echo $context->name; ?></a>-<?php echo $context->version['release']; ?>
+            <a href="<?php echo $frontend->getURL() . $context->name; ?>"><?php echo $context->name; ?></a>-<?php echo $context->version['release']; ?>
         </h2>
         <ul class="package-info">
             <li><strong>Version:</strong>
