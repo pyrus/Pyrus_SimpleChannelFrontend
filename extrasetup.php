a0ba1416063b7217d86817af97e1cf59c320cf80
Brett Bieber <brett.bieber@gmail.com>
2011-05-09 14:50:24 -0500

    Correct xml setup file for new PEAR2 package source directories.

--- extrasetup.php
+++ extrasetup.php
@@ -2,23 +2,14 @@
 /**
  * This file is used to provide extra files/packages outside package.xml
  */
-$pyrus = new \PEAR2\Pyrus\Package(__DIR__ . '/../../Pyrus/package.xml');
+$pyrus = new \PEAR2\Pyrus\Package(__DIR__ . '/../PEAR2_Pyrus/package.xml');
 $pyrus->setPackagingFilter('PEAR2\Pyrus\PackageFile\v2Iterator\MinimalPackageFilter');
-if (basename(__DIR__) == 'trunk') {
-    $extrafiles = array(
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../../HTTP_Request/trunk/package.xml'),
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../../sandbox/Console_CommandLine/trunk/package.xml'),
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../../MultiErrors/trunk/package.xml'),
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../../Exception/trunk/package.xml'),
-    );
-} else {
-    $extrafiles = array(
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../../HTTP_Request/package.xml'),
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../Console_CommandLine/package.xml'),
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../../MultiErrors/package.xml'),
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../../Exception/package.xml'),
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../../Exception/package.xml'),
-        $pyrus,
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../../Templates_Savant/package.xml'),
-    );
-}
+
+$extrafiles = array(
+    new \PEAR2\Pyrus\Package(__DIR__ . '/../PEAR2_HTTP_Request/package.xml'),
+    new \PEAR2\Pyrus\Package(__DIR__ . '/../PEAR2_Console_CommandLine/package.xml'),
+    new \PEAR2\Pyrus\Package(__DIR__ . '/../PEAR2_MultiErrors/package.xml'),
+    new \PEAR2\Pyrus\Package(__DIR__ . '/../PEAR2_Exception/package.xml'),
+    $pyrus,
+    new \PEAR2\Pyrus\Package(__DIR__ . '/../PEAR2_Templates_Savant/package.xml'),
+);
