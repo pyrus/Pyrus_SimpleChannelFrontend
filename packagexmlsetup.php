14f4fe151f74ddc95aa707c90dbcfa6b85b257a9
Brett Bieber <brett.bieber@gmail.com>
2011-05-09 14:50:45 -0500

    Ensure that PHP 5.3 is required

--- packagexmlsetup.php
+++ packagexmlsetup.php
@@ -14,6 +14,7 @@ $compatible->dependencies['required']->package['pear2.php.net/PEAR2_Exception']-
 $compatible->dependencies['required']->package['pear2.php.net/PEAR2_MultiErrors']->save();
 $compatible->dependencies['required']->package['pear2.php.net/PEAR2_HTTP_Request']->save();
 */
+$package->dependencies['required']->php = '5.3.2';
 $package->dependencies['required']->package['pear2.php.net/PEAR2_Pyrus']->save();
 $package->dependencies['required']->package['pear2.php.net/PEAR2_Templates_Savant']->save();
 
